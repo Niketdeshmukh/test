6fe1839fcf2ddf1cef1242f7f3673dfef448c259
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-06 12:54:03 +0530

    updated

--- cases/masterSuite.test.mjs
+++ cases/masterSuite.test.mjs
@@ -1,26 +1,26 @@
 // Import all test files sequentially
-// import './basic.test.mjs';
-// import './validateLogo.test.mjs';
-// import './logout.test.mjs'
+import './basic.test.mjs';
+import './validateLogo.test.mjs';
+import './logout.test.mjs'
 import './vuninstall.test.mjs';
-// import './Mobile App-Android_TCM_001.test.mjs'
-// import './Mobile App-Android_TCM_002.test.mjs'
+import './Mobile App-Android_TCM_001.test.mjs'
+import './Mobile App-Android_TCM_002.test.mjs'
 import './vv16.test.mjs';
-// import './32.test.mjs'
-// import './37.test.mjs'
-// import './validatingEmptyFeilds.test.mjs'
-// import './38.test.mjs'
-// import './43.test.mjs'
-// import './vv59.test.mjs';
-// import './vvvvchosedefaultvehicle.test.mjs'
-// import './vvdashboard.test.mjs'
-// import './renamemodel.test.mjs'
-// import './vvveditaddress.test.mjs'
-// import './50.test.mjs'
-// import './62.test.mjs'
-// import './DLuploading.test.mjs'
-// import './RCuploading.test.mjs'
-// import './ICuploading.test.mjs'
+import './32.test.mjs'
+import './37.test.mjs'
+import './validatingEmptyFeilds.test.mjs'
+import './38.test.mjs'
+import './43.test.mjs'
+import './vv59.test.mjs';
+import './vvvvchosedefaultvehicle.test.mjs'
+import './vvdashboard.test.mjs'
+import './renamemodel.test.mjs'
+import './vvveditaddress.test.mjs'
+import './50.test.mjs'
+import './62.test.mjs'
+import './DLuploading.test.mjs'
+import './RCuploading.test.mjs'
+import './ICuploading.test.mjs'
 
 describe('Complete Test Suite', function () {
   it('should run all test files in sequence', function () {
