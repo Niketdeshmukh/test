9c96464644ed6e0e35d7b82582796fc172368210
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-13 16:48:53 +0530

    merged

--- cases/loginLogout/login.test.mjs
+++ cases/loginLogout/login.test.mjs
@@ -1,6 +1,13 @@
 import { remote } from 'webdriverio';
 import { initializeDriver } from '../driverSetup.mjs';
 
+const OTP_DIGITS = ['1', '2', '3', '4'];
+const OTP_FIELDS_XPATHS = [
+  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText",
+  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText",
+  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[3]/android.widget.EditText",
+  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[4]/android.widget.EditText",
+];
 describe('Login Test', function () {
   let driver;
 
@@ -13,6 +20,7 @@ describe('Login Test', function () {
 
   it('should log in successfully with phone number', async function () {
     try {
+      this.timeout(60000);
       await driver.execute('mobile: activateApp', { appId: 'com.simpleenergy.app' });
       await driver.pause(2000);
       const signinButton = await waitForElement(driver, '//android.widget.Button', 10000);
@@ -37,6 +45,26 @@ describe('Login Test', function () {
 
       const submitButton = await waitForElement(driver, '//android.widget.Button', 5000);
       await submitButton.click();
+      for (let i = 0; i < OTP_DIGITS.length; i++) {
+        const otpField = await driver.$(OTP_FIELDS_XPATHS[i]);
+
+        if (await otpField.waitForExist({ timeout: 15000 })) {
+            console.log(`Entering OTP digit '${OTP_DIGITS[i]}' in field ${i + 1}`);
+            await otpField.setValue(OTP_DIGITS[i]);
+        } else {
+            console.warn(`OTP field ${i + 1} not found. Skipping.`);
+        }
+    }
+
+      const allowButton = await driver.$('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]');
+      await allowButton.waitForDisplayed({timeout:15000})
+      if (await allowButton.isDisplayed()) {
+        console.log('Allow button is visible. Clicking the button...');
+        await allowButton.waitForDisplayed({timeout:15000})
+        await allowButton.click();
+      } else {
+        console.log('Allow button is not visible. Ending the test with success message.');
+      }
     } catch (err) {
       console.error('An error occurred during the login test:', err.message);
     }
--- cases/loginLogout/otp.test.mjs
+++ cases/loginLogout/otp.test.mjs
@@ -1,13 +1,6 @@
 import { remote } from 'webdriverio';
 import { initializeDriver } from '../driverSetup.mjs';
 
-const OTP_DIGITS = ['1', '2', '3', '4'];
-const OTP_FIELDS_XPATHS = [
-  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText",
-  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText",
-  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[3]/android.widget.EditText",
-  "//androidx.compose.ui.platform.q1/android.view.View/android.view.View/android.view.View[4]/android.widget.EditText",
-];
 
 describe('Otp Test', function () {
   let driver;
@@ -23,26 +16,7 @@ describe('Otp Test', function () {
     this.timeout(30000);
     try {
 
-      for (let i = 0; i < OTP_DIGITS.length; i++) {
-        const otpField = await driver.$(OTP_FIELDS_XPATHS[i]);
-
-        if (await otpField.waitForExist({ timeout: 15000 })) {
-            console.log(`Entering OTP digit '${OTP_DIGITS[i]}' in field ${i + 1}`);
-            await otpField.setValue(OTP_DIGITS[i]);
-        } else {
-            console.warn(`OTP field ${i + 1} not found. Skipping.`);
-        }
-    }
-
-      const allowButton = await driver.$('//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]');
-      await allowButton.waitForDisplayed({timeout:15000})
-      if (await allowButton.isDisplayed()) {
-        console.log('Allow button is visible. Clicking the button...');
-        await allowButton.waitForDisplayed({timeout:15000})
-        await allowButton.click();
-      } else {
-        console.log('Allow button is not visible. Ending the test with success message.');
-      }
+      
 
     } catch (err) {
       console.error('An error occurred during the login test:', err.message);
