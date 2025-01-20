1c51f5100869f5a204406d931322c1f42c3f2b4e
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-20 12:25:57 +0530

    updated

--- cases/masterSuite.test.mjs
+++ cases/masterSuite.test.mjs
@@ -1,29 +1,29 @@
 import './basic.test.mjs';
 import './validate/validateLogo.test.mjs';
-import './loginLogout/logout.test.mjs'
-import './others/uninstallReinstall.test.mjs';
-import './validate/WalkthroughScreen.test.mjs'
-import './loginLogout/login.test.mjs'
-import './validate/validateWidgets.test.mjs'
-import './validate/validateProfileButton.test.mjs'
-import './validate/emailLengthvalidate.test.mjs'
-import './validate/validatingEmptyFeilds.test.mjs'
-// import './editDetails/changeProfile.test.mjs'
-import './editDetails/editNameEmail.test.mjs'
-import './validate/testSampleEmails.test.mjs';
-import './others/choseDefaultVehicle.test.mjs'
-import './validate/validateDateAndStats.test.mjs'
-import './editDetails/renameModel.test.mjs'
-import './editDetails/editAddress.test.mjs'
-import './editDetails/clearAddress.test.mjs'
-// import './documentUpload/DLuploading.test.mjs'
-// import './documentUpload/RCuploading.test.mjs'
-// import './documentUpload/ICuploading.test.mjs'
-// import './others/screenOrientation.test.mjs'
-import "./externalWidgets/termsAndContitions.test.mjs";
-import "./externalWidgets/ownersManual.test.mjs"
-import "./validate/validateRsa.test.mjs";
-// import './externalWidgets/bookService.test.mjs'
+// import './loginLogout/logout.test.mjs'
+// import './others/uninstallReinstall.test.mjs';
+// import './validate/WalkthroughScreen.test.mjs'
+// import './loginLogout/login.test.mjs'
+// import './validate/validateWidgets.test.mjs'
+// import './validate/validateProfileButton.test.mjs'
+// import './validate/emailLengthvalidate.test.mjs'
+// import './validate/validatingEmptyFeilds.test.mjs'
+// // import './editDetails/changeProfile.test.mjs'
+// import './editDetails/editNameEmail.test.mjs'
+// import './validate/testSampleEmails.test.mjs';
+// import './others/choseDefaultVehicle.test.mjs'
+// import './validate/validateDateAndStats.test.mjs'
+// import './editDetails/renameModel.test.mjs'
+// import './editDetails/editAddress.test.mjs'
+// import './editDetails/clearAddress.test.mjs'
+// // import './documentUpload/DLuploading.test.mjs'
+// // import './documentUpload/RCuploading.test.mjs'
+// // import './documentUpload/ICuploading.test.mjs'
+// // import './others/screenOrientation.test.mjs'
+// import "./externalWidgets/termsAndContitions.test.mjs";
+// import "./externalWidgets/ownersManual.test.mjs"
+// import "./validate/validateRsa.test.mjs";
+// // import './externalWidgets/bookService.test.mjs'
 
 describe('Complete Test Suite', function () {
   it('should run all test files in sequence', function () {
